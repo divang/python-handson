d225ce07546d595bbf5fa1f657f2933e6098d5c5
Divang Sharma <divang.s@gmail.com>
2022-06-22 20:41:34 +0530

    Add files via upload

---
+++ my_first_loop.py
@@ -0,0 +1,18 @@
+import sys
+table_num=int(sys.argv[1])
+print(1*table_num)
+print(2*table_num)
+print(3*table_num)
+print(4*table_num)
+print(5*table_num)
+print(6*table_num)
+print(7*table_num)
+print(8*table_num)
+print(9*table_num)
+print(10*table_num)
+
+print("------ By loop ------ ")
+i=1
+while i <= 10:
+	print(i*table_num)
+	i = i + 1
